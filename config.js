00f2f8ccb04f2bbfd315a8a36b0c230012793d25
Itzreal09 <101443633+Itzreal09@users.noreply.github.com>
2022-04-28 14:09:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
         px: '!',
-        playing: '*Itzme ❤️ Music Bot',
+        playing: '!help ',
 
     opt: {
         DJ: {
-            enabled: false, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
+            enabled: true , //IF YOU WANT ONLY DJS TO USE IT, set false to true.
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
